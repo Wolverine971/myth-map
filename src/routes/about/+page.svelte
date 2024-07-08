26ec0e03baa3b8b8d43477713f8f035235418997
david.t.wayne <david.t.wayne.ctr@usmc.mil>
2024-07-08 13:59:28 -0400

    updates

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,24 +7,22 @@
 <div style="text-align: start;">
 	<Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold md:text-5xl">Mission</Heading>
 	<P class="mb-4">
-		Tiny Tribe Adventures, is on a quest to help families to create unforgettable memories through
+		Tiny Tribe Adventures is on a quest to help families create unforgettable memories through
 		adventuring.
 	</P>
 	<P class="mb-4">
-		Parenting is hard, we get it, but it doesn’t have to be lonely. Creating experiences with your
+		Parenting is hard, we get it, but it doesn't have to be lonely. Creating experiences with your
 		family and building connections with others should be easy and fun. We want to help every family
 		do just that by making it easier to find adventures that speak to everyone in the family, not
 		just the kids.
 	</P>
-	<P class="mb-4"
-		>Find your next adventure spot, put down your phone, and let’s go exploring together!</P
-	>
+	<P class="mb-4">Find your next adventure spot, put down your phone, and explore together!</P>
 
 	<Heading tag="h2" class="mb-4" customSize="text-4xl font-extrabold md:text-5xl">About Us</Heading>
 	<P class="mb-4">
-		We are two families that met randomly at a kids birthday party and became fast friends. Many
-		stars aligned for us to meet and engage with each other, and it inspired us to help other
-		families expand their own tribe.
+		We are two families that met randomly at a kid's birthday party and became fast friends. Many
+		stars aligned for us to meet and engage with each other, inspiring us to help other families
+		expand their own tribe.
 	</P>
 	<P class="mb-4">This is a project by Zach and Kerry, DJ and Anna 👋</P>
 	<!-- <P class="mb-4">
